1acbb4a91bf1722bc31bfe14982cb9a2c0d2d777
knowpage <trangnt23042000@gmail.com>
2024-12-14 10:19:29 +0900

    lec 3 - first commit

---
+++ flutter_class_lec3/lib/code_container_widget.dart
@@ -0,0 +1,142 @@
+// Lec 3: Container class
+
+// // Container class - Properties - child
+// import 'package:flutter/material.dart';
+
+// void main() => runApp(const MyApp());
+
+// class MyApp extends StatelessWidget {
+// const MyApp({Key? key}) : super(key: key);
+
+// @override
+// Widget build(BuildContext context) {
+// 	return MaterialApp(
+// 	home: Scaffold(
+// 		appBar: AppBar(
+// 		title: const Text("Container example"),
+// 		),
+// 		body: Container(
+// 		child:const Text("Hello! i am inside a container!",
+// 			style: TextStyle(fontSize: 20)),
+// 		),
+// 	),
+// 	);
+// }
+// }
+
+
+// // Container class - Properties - color
+// import 'package:flutter/material.dart';
+
+// void main() => runApp(const MyApp());
+
+// class MyApp extends StatelessWidget {
+// const MyApp({Key? key}) : super(key: key);
+
+// @override
+// Widget build(BuildContext context) {
+// 	return MaterialApp(
+// 		home
+// 		: Scaffold(appBar
+// 				: AppBar(title
+// 							: const Text("Container example"),
+// 						),
+// 					body
+// 				: Container(color
+// 							: Colors.purple,
+// 							child
+// 							: const Text("Hello! i am inside a container!",
+// 									style
+// 									: TextStyle(fontSize : 20)),
+// 							),
+// 				), 
+// 	);
+// }
+// }
+
+
+// // Container class - Properties - height and width
+// import 'package:flutter/material.dart';
+
+// void main() => runApp(const MyApp());
+
+// class MyApp extends StatelessWidget {
+// const MyApp({Key? key}) : super(key: key);
+
+// @override
+// Widget build(BuildContext context) {
+// 	return MaterialApp(
+// 	home: Scaffold(
+// 		appBar: AppBar(
+// 		title: const Text("Container example"),
+// 		),
+// 		body: Container(
+// 		height: 200,
+// 		width: double.infinity,
+// 		color: Colors.purple,
+// 		child: const Text("Hello! i am inside a container!",
+// 			style: TextStyle(fontSize: 20)),
+// 		),
+// 	),
+// 	);
+// }
+// }
+
+// // Container class - Properties - margin
+// import 'package:flutter/material.dart';
+
+// void main() => runApp(const MyApp());
+
+// class MyApp extends StatelessWidget {
+// const MyApp({Key? key}) : super(key: key);
+
+// @override
+// Widget build(BuildContext context) {
+// 	return MaterialApp(
+// 	home: Scaffold(
+// 		appBar: AppBar(
+// 		title: const Text("Container example"),
+// 		),
+// 		body: Container(
+// 		height: 200,
+// 		width: double.infinity,
+// 		color: Colors.purple,
+// 		margin: const EdgeInsets.all(20),
+// 		child: const Text("Hello! i am inside a container!",
+// 			style: TextStyle(fontSize: 20)),
+// 		),
+// 	),
+// 	);
+// }
+// }
+
+// Container class - Properties - padding
+import 'package:flutter/material.dart';
+
+void main() => runApp(const MyApp());
+
+class MyApp extends StatelessWidget {
+const MyApp({Key? key}) : super(key: key);
+
+@override
+Widget build(BuildContext context) {
+	return MaterialApp(
+	home: Scaffold(
+		appBar: AppBar(
+		title: const Text("Container example"),
+		),
+		body: Container(
+		height: 200,
+		width: double.infinity,
+		color: Colors.purple,
+		margin: const EdgeInsets.all(20),
+		padding: const EdgeInsets.all(30),
+		child: const Text("Hello! i am inside a container!",
+			style: TextStyle(fontSize: 20)),
+		),
+	),
+	);
+}
+}
+
+
